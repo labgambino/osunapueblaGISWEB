2099023329a6cee8491f79aa3fa8fd22c68c9aab
labgambino <113803192+labgambino@users.noreply.github.com>
2024-07-12 20:03:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+setTimeout(function(){ alert("powered by BERMUDEZ (2024)"); }, 3000);
+
+function ir (){
+    
+var c=2244; 
+var u="notorius"; 
+    if(document.form.password.value==c && document.form.login.value==u){
+    alert ("ATTEC: Acceso permitido al EXPEDIENTE 202X 9X2X0X. \nTodos los Derechos reservados."); 
+window.location="hiperenlopag.html";} 
+    
+    else { 
+        alert("Por favor, ingresa tu usuario y contraseña correctos");
+    }
+     }
+
