77ce65aa5d28be9148bb33e7605ef9f21b215700
labgambino <113803192+labgambino@users.noreply.github.com>
2024-07-12 20:02:19 +0200

    Add files via upload

---
+++ layers/ARQUETASentities_11.js
@@ -0,0 +1,14 @@
+var json_ARQUETASentities_11 = {
+"type": "FeatureCollection",
+"name": "ARQUETASentities_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Layer": "IN_09_EP_TEXTOS", "PaperSpace": "true", "SubClasses": "AcDbEntity:AcDbMText", "Linetype": null, "EntityHandle": "865", "Text": "TITULO DEL PLANO PARRAFO 1\nTITULO DE PLANO PARRAFO 2 " }, "geometry": { "type": "Point", "coordinates": [ -7.482185710386138, 0.000332718264081 ] } },
+{ "type": "Feature", "properties": { "Layer": "IN_09_EP_TEXTOS", "PaperSpace": "true", "SubClasses": "AcDbEntity:AcDbMText", "Linetype": null, "EntityHandle": "867", "Text": "ESCALA 1\nESCALA 2" }, "geometry": { "type": "Point", "coordinates": [ -7.481404084896826, 0.000174577433472 ] } },
+{ "type": "Feature", "properties": { "Layer": "IN_09_EP_TEXTOS", "PaperSpace": "true", "SubClasses": "AcDbEntity:AcDbMText", "Linetype": null, "EntityHandle": "869", "Text": "01" }, "geometry": { "type": "Point", "coordinates": [ -7.481515017271885, 0.000252562868217 ] } },
+{ "type": "Feature", "properties": { "Layer": "IN_09_EP_TEXTOS", "PaperSpace": "true", "SubClasses": "AcDbEntity:AcDbMText", "Linetype": null, "EntityHandle": "86B", "Text": "01" }, "geometry": { "type": "Point", "coordinates": [ -7.481443301965864, 0.000252562893162 ] } },
+{ "type": "Feature", "properties": { "Layer": "IN_09_EP_TEXTOS", "PaperSpace": "true", "SubClasses": "AcDbEntity:AcDbMText", "Linetype": null, "EntityHandle": "86D", "Text": "1" }, "geometry": { "type": "Point", "coordinates": [ -7.481454015310022, 0.000322818624729 ] } },
+{ "type": "Feature", "properties": { "Layer": "IN_09_EP_TEXTOS", "PaperSpace": "true", "SubClasses": "AcDbEntity:AcDbMText", "Linetype": null, "EntityHandle": "86F", "Text": "JUNIO 2018" }, "geometry": { "type": "Point", "coordinates": [ -7.481517676690799, 0.000434389703403 ] } },
+{ "type": "Feature", "properties": { "Layer": "IN_09_EP_TEXTOS", "PaperSpace": "true", "SubClasses": "AcDbEntity:AcDbMText", "Linetype": null, "EntityHandle": "871", "Text": "0" }, "geometry": { "type": "Point", "coordinates": [ -7.481588167527973, 0.000434389661232 ] } }
+]
+}
